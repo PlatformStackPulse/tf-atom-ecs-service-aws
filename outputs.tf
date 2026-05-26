4a997fdc171145156d80bd20e0ad3b71f44369d0
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:14:31 +0100

    feat: implement tf-atom-ecs-service-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "id" {
+  description = "ID of the ECS service"
+  value       = try(aws_ecs_service.this[0].id, null)
+}
+
+output "name" {
+  description = "Name of the ECS service"
+  value       = try(aws_ecs_service.this[0].name, null)
+}
+
+output "cluster" {
+  description = "Cluster ARN the service runs on"
+  value       = try(aws_ecs_service.this[0].cluster, null)
 }
